08b26174a65c72c1cee3921d6642206880d81187
K1unk <klunk@inbox.ru>
2019-03-02 16:22:32 +0300

    добавлены тесты и пояснения

--- src/main/java/tree.java
+++ src/main/java/tree.java
@@ -16,40 +16,38 @@ public class tree {
 
     private Node MainParent;
 
+    //добавление первого элемента в дерево
     public Node addInTree(int number) {
         if (MainParent == null) {
             MainParent = new Node(number, null, null, null);
             return MainParent;
-        }
-        else {
+        } else {
             return addOthers(number, MainParent);
         }
     }
 
+    //добавление остальных элементов
     public Node addOthers(int number, Node root) {
         if (root.number > number) {
             if (root.left == null) {
-                root.left =  new Node(number, root, null, null);
+                root.left = new Node(number, root, null, null);
                 return root.left;
-            }
-            else {
+            } else {
                 addOthers(number, root.left);
             }
-        }
-        else {
+        } else {
             if (root.right == null) {
                 root.right = new Node(number, root, null, null);
                 return root.right;
-            }
-            else {
+            } else {
                 addOthers(number, root.right);
             }
         }
         return null;
     }
 
+    // поиск элемента в дереве
     public Node SearchRoot(int number) {
-
         int numberOfRoot = MainParent.number;
         Node root = MainParent;
 
@@ -58,8 +56,7 @@ public class tree {
                 root = root.right;
                 if (root == null) throw new IllegalArgumentException();
                 numberOfRoot = root.number;
-            }
-            else {
+            } else {
                 root = root.left;
                 if (root == null) throw new IllegalArgumentException();
                 numberOfRoot = root.number;
@@ -69,96 +66,99 @@ public class tree {
 
     }
 
+    //поиск всех соседних корней данного элемента
     public Node SearchParent(int number) {
         return SearchRoot(number).parent;
     }
 
-    public Node SearchLeft (int number) {
+    public Node SearchLeft(int number) {
         return SearchRoot(number).left;
     }
 
-    public Node SearchRight (int number) {
+    public Node SearchRight(int number) {
         return SearchRoot(number).right;
     }
 
+    //удаление корня дерева
     public final boolean Delete(int number) {
         Node root = SearchRoot(number);
 
-            if (root.left == null && root.right == null) {                  //нет "детей"
+        if (root.left == null && root.right == null) {                  //нет "детей"
+            if (root.parent.right == root) {
+                root.parent.right = null;
+                return true;
+            } else {
+                root.parent.left = null;
+                return true;
+            }
+        } else {
+            if (root.left != null && root.right == null) {              //только левый ребенок
                 if (root.parent.right == root) {
-                    root.parent.right = null;
+                    root.parent.right = root.left;
+                    root.left.parent = root.parent;
                     return true;
                 } else {
-                    root.parent.left = null;
+                    root.parent.left = root.left;
+                    root.left.parent = root.parent;
                     return true;
                 }
             } else {
-                if (root.left != null && root.right == null) {              //только левый ребенок
+                if (root.left == null && root.right != null) {          //только правый ребенок
                     if (root.parent.right == root) {
-                        root.parent.right = root.left;
-                        root.left.parent = root.parent;
+                        root.parent.right = root.right;
+                        root.right.parent = root.parent;
                         return true;
                     } else {
-                        root.parent.left = root.left;
-                        root.left.parent = root.parent;
+                        root.parent.left = root.right;
+                        root.right.parent = root.parent;
                         return true;
                     }
-                } else {
-                    if (root.left == null && root.right != null) {          //только правый ребенок
-                        if (root.parent.right == root) {
-                            root.parent.right = root.right;
-                            root.right.parent = root.parent;
-                            return true;
-                        } else {
-                            root.parent.left = root.right;
-                            root.right.parent = root.parent;
-                            return true;
+                } else {                                                //оба ребенка
+                    if (root.number - root.left.number > root.right.number - root.number) {
+                        Node rootInSearchingNull = root.right;
+                        while (rootInSearchingNull.left != null) {
+                            rootInSearchingNull = rootInSearchingNull.left;
                         }
-                    } else {                                                //оба ребенка
-                        if (root.number - root.left.number > root.right.number - root.number) {
-                            Node rootInSearchingNull = root.right;
-                            while (rootInSearchingNull.left != null) {
-                                rootInSearchingNull = rootInSearchingNull.left;
-                            }
-                            rootInSearchingNull.left = root.left;
-                            root.left.parent = rootInSearchingNull;
-                            return true;
-                        } else {
-                            Node rootInSearchingNull = root.left;
-                            while (rootInSearchingNull.right != null) {
-                                rootInSearchingNull = rootInSearchingNull.right;
-                            }
-                            rootInSearchingNull.right = root.right;
-                            root.right.parent = rootInSearchingNull;
-                            return true;
+                        rootInSearchingNull.left = root.left;
+                        root.left.parent = rootInSearchingNull;
+                        return true;
+                    } else {
+                        Node rootInSearchingNull = root.left;
+                        while (rootInSearchingNull.right != null) {
+                            rootInSearchingNull = rootInSearchingNull.right;
                         }
+                        rootInSearchingNull.right = root.right;
+                        root.right.parent = rootInSearchingNull;
+                        return true;
                     }
                 }
             }
         }
+    }
 
-
+    //выводит строку вида "Номер данного элемента, номер элемента родителя, номер левого ребенка, номер правого ребенка"
     public String toString(int number) {
         StringBuilder sb = new StringBuilder();
         sb.append("Number: ");
         sb.append(number);
         sb.append(", parent: ");
-        sb.append(SearchParent(number).number);
+        if (SearchParent(number) != null) {
+            sb.append(SearchParent(number).number);
+        } else {
+            sb.append("null");
+        }
         sb.append(", left: ");
         if (SearchLeft(number) != null) {
             sb.append(SearchLeft(number).number);
-        }
-        else {
+        } else {
             sb.append("null");
         }
         sb.append(", right: ");
         if (SearchLeft(number) != null) {
             sb.append(SearchRight(number).number);
-        }
-        else {
+        } else {
             sb.append("null");
         }
         return sb.toString();
     }
-
 }
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -4,7 +4,7 @@ import static junit.framework.TestCase.fail;
 
 public class test {
     @Test
-    public void Insert_Search_Test() {
+    public void test1() {
         tree d = new tree();
 
         d.addInTree(5);
@@ -12,7 +12,6 @@ public class test {
         d.addInTree(3);
         d.addInTree(4);
         d.addInTree(7);
-
     }
 
     @Test
@@ -24,8 +23,22 @@ public class test {
         d.addInTree(3);
         d.addInTree(4);
         d.addInTree(7);
-        d.Delete(5);
-        assertEquals("Number: 3, parent: 6, left: null, right: null", d.toString(3));
+        d.Delete(3);
+        assertEquals("Number: 4, parent: 5, left: null, right: null", d.toString(4));
+    }
 
+    @Test
+    public void test3() {
+        tree d = new tree();
+
+        d.addInTree(8);
+        d.addInTree(9);
+        d.addInTree(10);
+        d.addInTree(11);
+        d.addInTree(1);
+        d.Delete(9);
+        d.Delete(10);
+        assertEquals("Number: 8, parent: null, left: 1, right: 11", d.toString(8));
+        assertEquals("Number: 11, parent: 8, left: null, right: null", d.toString(11));
     }
 }
